d36141209d4ac833e7a48b390e326ff885490996
nvmd <phd.pc@hotmail.com>
2020-06-23 17:50:23 -0300

    clang-format run

--- emac/eth_mac.c
+++ emac/eth_mac.c
@@ -20,302 +20,307 @@
 #include "w5100_socket.h"
 
 static const char *TAG = "w5100_eth_mac";
-#define MAC_CHECK(a, str, goto_tag, ret_value, ...)                               \
-    do                                                                            \
-    {                                                                             \
-        if (!(a))                                                                 \
-        {                                                                         \
-            ESP_LOGE(TAG, "%s(%d): " str, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
-            ret = ret_value;                                                      \
-            goto goto_tag;                                                        \
-        }                                                                         \
-    } while (0)
+#define MAC_CHECK( a, str, goto_tag, ret_value, ... )                               \
+	do                                                                              \
+	{                                                                               \
+		if ( !( a ) )                                                               \
+		{                                                                           \
+			ESP_LOGE( TAG, "%s(%d): " str, __FUNCTION__, __LINE__, ##__VA_ARGS__ ); \
+			ret = ret_value;                                                        \
+			goto goto_tag;                                                          \
+		}                                                                           \
+	} while ( 0 )
 
-typedef struct {
-    esp_eth_mac_t parent;
-    esp_eth_mediator_t *eth;
-    TaskHandle_t rx_task_hdl;
-    uint8_t addr[6];
+typedef struct
+{
+	esp_eth_mac_t parent;
+	esp_eth_mediator_t *eth;
+	TaskHandle_t rx_task_hdl;
+	uint8_t addr[ 6 ];
 } emac_w5100_t;
 
 /**
  * @brief Write ENC28J60 internal PHY register
  */
-static esp_err_t emac_w5100_write_phy_reg(esp_eth_mac_t *mac, uint32_t phy_addr,
-        uint32_t phy_reg, uint32_t reg_value)
+static esp_err_t emac_w5100_write_phy_reg( esp_eth_mac_t *mac, uint32_t phy_addr, uint32_t phy_reg, uint32_t reg_value )
 {
 	START;
 	END;
-    return ESP_OK;
+	return ESP_OK;
 }
 
 /**
  * @brief Read ENC28J60 internal PHY register
  */
-static esp_err_t emac_w5100_read_phy_reg(esp_eth_mac_t *mac, uint32_t phy_addr,
-        uint32_t phy_reg, uint32_t *reg_value)
+static esp_err_t emac_w5100_read_phy_reg( esp_eth_mac_t *mac, uint32_t phy_addr, uint32_t phy_reg, uint32_t *reg_value )
 {
 	START;
 	END;
-    return ESP_OK;
+	return ESP_OK;
 }
 
 /**
  * @brief Set mediator for Ethernet MAC
  */
-static esp_err_t emac_w5100_set_mediator(esp_eth_mac_t *mac, esp_eth_mediator_t *eth)
+static esp_err_t emac_w5100_set_mediator( esp_eth_mac_t *mac, esp_eth_mediator_t *eth )
 {
 	START;
-    esp_err_t ret = ESP_OK;
-    MAC_CHECK(eth, "can't set mac's mediator to null", out, ESP_ERR_INVALID_ARG);
-    emac_w5100_t *emac = __containerof(mac, emac_w5100_t, parent);
-    emac->eth = eth;
+	esp_err_t ret = ESP_OK;
+	MAC_CHECK( eth, "can't set mac's mediator to null", out, ESP_ERR_INVALID_ARG );
+	emac_w5100_t *emac = __containerof( mac, emac_w5100_t, parent );
+	emac->eth = eth;
 out:
 	END;
-    return ret;
+	return ret;
 }
 
 /**
  * @brief   Stop w5100: disable interrupt and stop receiving packets
  */
-static esp_err_t w5100_stop(emac_w5100_t *emac)
+static esp_err_t w5100_stop( emac_w5100_t *emac )
 {
 	START;
-    close();
+	close();
 	END;
-    return ESP_OK;
+	return ESP_OK;
 }
 
-static esp_err_t emac_w5100_set_addr(esp_eth_mac_t *mac, uint8_t *addr)
+static esp_err_t emac_w5100_set_addr( esp_eth_mac_t *mac, uint8_t *addr )
 {
 	START;
-    esp_err_t ret = ESP_OK;
-    MAC_CHECK(addr, "can't set mac addr to null", out, ESP_ERR_INVALID_ARG);
-    emac_w5100_t *emac = __containerof(mac, emac_w5100_t, parent);
-    memcpy(emac->addr, addr, 6);
-    wiz_write_buf(SHAR0, addr, 6);
+	esp_err_t ret = ESP_OK;
+	MAC_CHECK( addr, "can't set mac addr to null", out, ESP_ERR_INVALID_ARG );
+	emac_w5100_t *emac = __containerof( mac, emac_w5100_t, parent );
+	memcpy( emac->addr, addr, 6 );
+	wiz_write_buf( SHAR0, addr, 6 );
 out:
 	END;
-    return ret;
+	return ret;
 }
 
-static esp_err_t emac_w5100_get_addr(esp_eth_mac_t *mac, uint8_t *addr)
+static esp_err_t emac_w5100_get_addr( esp_eth_mac_t *mac, uint8_t *addr )
 {
 	START;
-    esp_err_t ret = ESP_OK;
-    MAC_CHECK(addr, "can't set mac addr to null", out, ESP_ERR_INVALID_ARG);
-    emac_w5100_t *emac = __containerof(mac, emac_w5100_t, parent);
-    memcpy(addr, emac->addr, 6);
+	esp_err_t ret = ESP_OK;
+	MAC_CHECK( addr, "can't set mac addr to null", out, ESP_ERR_INVALID_ARG );
+	emac_w5100_t *emac = __containerof( mac, emac_w5100_t, parent );
+	memcpy( addr, emac->addr, 6 );
 out:
 	END;
-    return ret;
+	return ret;
 }
 
-static void emac_w5100_task(void *arg)
+static void emac_w5100_task( void *arg )
 {
-    emac_w5100_t *emac = (emac_w5100_t *)arg;
-    uint8_t status = 0;
-    uint8_t *buffer = NULL;
-    uint32_t length = 0;
+	emac_w5100_t *emac = ( emac_w5100_t * )arg;
+	uint8_t status = 0;
+	uint8_t *buffer = NULL;
+	uint32_t length = 0;
 
-	ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+	ulTaskNotifyTake( pdTRUE, portMAX_DELAY );
 
-    while (1)
+	while ( 1 )
 	{
 		// read interrupt status
-        status = getS0_IR();
+		status = getS0_IR();
 
 		/* packet received */
-        if (status & S0_IR_RECV)
+		if ( status & S0_IR_RECV )
 		{
 			length = getS0_RX_RSR();
-			buffer = malloc(length);
-			if (emac->parent.receive(&emac->parent, buffer, &length) == ESP_OK)
+			buffer = malloc( length );
+			if ( emac->parent.receive( &emac->parent, buffer, &length ) == ESP_OK )
 			{
 				/* pass the buffer to stack (e.g. TCP/IP layer) */
-				if (length)
-					ESP_ERROR_CHECK(emac->eth->stack_input(emac->eth, buffer, length));
+				if ( length )
+					ESP_ERROR_CHECK( emac->eth->stack_input( emac->eth, buffer, length ) );
 				else
-					ESP_LOGE(TAG, "LENGTH = 0");
+					ESP_LOGE( TAG, "LENGTH = 0" );
 			}
 			else
-				ESP_LOGE(TAG, "Failed to recv data from ETH");
+				ESP_LOGE( TAG, "Failed to recv data from ETH" );
 
 			// free(buffer);
 			// buffer = NULL;
 		}
-    }
-    vTaskDelete(NULL);
+	}
+	vTaskDelete( NULL );
 }
 
-static esp_err_t emac_w5100_set_link(esp_eth_mac_t *mac, eth_link_t link)
+static esp_err_t emac_w5100_set_link( esp_eth_mac_t *mac, eth_link_t link )
 {
 	START;
-    esp_err_t ret = ESP_OK;
-    emac_w5100_t *emac = __containerof(mac, emac_w5100_t, parent);
-    switch (link) {
-    case ETH_LINK_UP:
-		// socket(true);
-        break;
-    case ETH_LINK_DOWN:
-        // MAC_CHECK(w5100_stop(emac) == ESP_OK, "w5100 stop failed", out, ESP_FAIL);
-        break;
-    default:
-        MAC_CHECK(false, "unknown link status", out, ESP_ERR_INVALID_ARG);
-        break;
-    }
+	esp_err_t ret = ESP_OK;
+	emac_w5100_t *emac = __containerof( mac, emac_w5100_t, parent );
+	switch ( link )
+	{
+		case ETH_LINK_UP:
+			// socket(true);
+			break;
+		case ETH_LINK_DOWN:
+			// MAC_CHECK(w5100_stop(emac) == ESP_OK, "w5100 stop failed", out, ESP_FAIL);
+			break;
+		default:
+			MAC_CHECK( false, "unknown link status", out, ESP_ERR_INVALID_ARG );
+			break;
+	}
 out:
 	END;
-    return ret;
+	return ret;
 }
 
-static esp_err_t emac_w5100_set_speed(esp_eth_mac_t *mac, eth_speed_t speed)
+static esp_err_t emac_w5100_set_speed( esp_eth_mac_t *mac, eth_speed_t speed )
 {
 	START;
 	END;
-    return ESP_OK;
+	return ESP_OK;
 }
 
-static esp_err_t emac_w5100_set_duplex(esp_eth_mac_t *mac, eth_duplex_t duplex)
+static esp_err_t emac_w5100_set_duplex( esp_eth_mac_t *mac, eth_duplex_t duplex )
 {
 	START;
 	END;
-    return ESP_OK;
+	return ESP_OK;
 }
 
-static esp_err_t emac_w5100_set_promiscuous(esp_eth_mac_t *mac, bool enable)
+static esp_err_t emac_w5100_set_promiscuous( esp_eth_mac_t *mac, bool enable )
 {
 	START;
-    emac_w5100_t *emac = __containerof(mac, emac_w5100_t, parent);
+	emac_w5100_t *emac = __containerof( mac, emac_w5100_t, parent );
 
-	uint8_t mode_register = IINCHIP_READ(S0_MR);
+	uint8_t mode_register = IINCHIP_READ( S0_MR );
 
-    if (enable && !(mode_register & S0_MR_MF))
-		IINCHIP_WRITE(S0_MR, mode_register | S0_MR_MF);
-	else if (!enable && (mode_register & S0_MR_MF))
-		IINCHIP_WRITE(S0_MR, mode_register & ~S0_MR_MF);
+	if ( enable && !( mode_register & S0_MR_MF ) )
+		IINCHIP_WRITE( S0_MR, mode_register | S0_MR_MF );
+	else if ( !enable && ( mode_register & S0_MR_MF ) )
+		IINCHIP_WRITE( S0_MR, mode_register & ~S0_MR_MF );
 
 	END;
-    return ESP_OK;
+	return ESP_OK;
 }
 
-static esp_err_t emac_w5100_transmit(esp_eth_mac_t *mac, uint8_t *buf, uint32_t length)
+static esp_err_t emac_w5100_transmit( esp_eth_mac_t *mac, uint8_t *buf, uint32_t length )
 {
 	START;
-    esp_err_t ret = ESP_OK;
-    emac_w5100_t *emac = __containerof(mac, emac_w5100_t, parent);
+	esp_err_t ret = ESP_OK;
+	emac_w5100_t *emac = __containerof( mac, emac_w5100_t, parent );
 
 	// ESP_LOG_BUFFER_HEXDUMP("", buf, length, ESP_LOG_WARN);
 
-	while (length > SSIZE)
+	while ( length > SSIZE )
 	{
-		sendto(buf, SSIZE);
+		sendto( buf, SSIZE );
 		length -= SSIZE;
 	}
 
-	sendto(buf, (uint16_t)length);
+	sendto( buf, ( uint16_t )length );
 	END;
-    return ret;
+	return ret;
 }
 
-static esp_err_t emac_w5100_receive(esp_eth_mac_t *mac, uint8_t *buf, uint32_t *length)
+static esp_err_t emac_w5100_receive( esp_eth_mac_t *mac, uint8_t *buf, uint32_t *length )
 {
 	START;
-    esp_err_t ret = ESP_OK;
-    emac_w5100_t *emac = __containerof(mac, emac_w5100_t, parent);
+	esp_err_t ret = ESP_OK;
+	emac_w5100_t *emac = __containerof( mac, emac_w5100_t, parent );
 
-	*length = recvfrom(buf);
+	*length = recvfrom( buf );
 
 	// ESP_LOG_BUFFER_HEXDUMP("", buf, *length, ESP_LOG_INFO);
 	END;
-    return ret;
+	return ret;
 }
 
-static esp_err_t emac_w5100_init(esp_eth_mac_t *mac)
+static esp_err_t emac_w5100_init( esp_eth_mac_t *mac )
 {
 	START;
-    esp_err_t ret = ESP_OK;
-    emac_w5100_t *emac = __containerof(mac, emac_w5100_t, parent);
-    esp_eth_mediator_t *eth = emac->eth;
+	esp_err_t ret = ESP_OK;
+	emac_w5100_t *emac = __containerof( mac, emac_w5100_t, parent );
+	esp_eth_mediator_t *eth = emac->eth;
 
-	uint8_t mac_addr[6];
-	ESP_ERROR_CHECK(esp_read_mac(mac_addr, ESP_MAC_ETH));
-	iinchip_init(mac_addr);
-	memcpy(emac->addr, mac_addr, 6);
+	uint8_t mac_addr[ 6 ];
+	ESP_ERROR_CHECK( esp_read_mac( mac_addr, ESP_MAC_ETH ) );
+	iinchip_init( mac_addr );
+	memcpy( emac->addr, mac_addr, 6 );
 
-    MAC_CHECK(eth->on_state_changed(eth, ETH_STATE_LLINIT, NULL) == ESP_OK,
-              "lowlevel init failed", out, ESP_FAIL);
+	MAC_CHECK( eth->on_state_changed( eth, ETH_STATE_LLINIT, NULL ) == ESP_OK, "lowlevel init failed", out, ESP_FAIL );
 
-	xTaskNotifyGive(emac->rx_task_hdl);
+	xTaskNotifyGive( emac->rx_task_hdl );
 
 	END;
-    return ESP_OK;
+	return ESP_OK;
 out:
-    eth->on_state_changed(eth, ETH_STATE_DEINIT, NULL);
+	eth->on_state_changed( eth, ETH_STATE_DEINIT, NULL );
 	END;
-    return ret;
+	return ret;
 }
 
-static esp_err_t emac_w5100_deinit(esp_eth_mac_t *mac)
+static esp_err_t emac_w5100_deinit( esp_eth_mac_t *mac )
 {
 	START;
-    emac_w5100_t *emac = __containerof(mac, emac_w5100_t, parent);
-    esp_eth_mediator_t *eth = emac->eth;
-    w5100_stop(emac);
+	emac_w5100_t *emac = __containerof( mac, emac_w5100_t, parent );
+	esp_eth_mediator_t *eth = emac->eth;
+	w5100_stop( emac );
 
 	// TODO
 
-    eth->on_state_changed(eth, ETH_STATE_DEINIT, NULL);
+	eth->on_state_changed( eth, ETH_STATE_DEINIT, NULL );
 	END;
-    return ESP_OK;
+	return ESP_OK;
 }
 
-static esp_err_t emac_w5100_del(esp_eth_mac_t *mac)
+static esp_err_t emac_w5100_del( esp_eth_mac_t *mac )
 {
 	START;
-    emac_w5100_t *emac = __containerof(mac, emac_w5100_t, parent);
-    vTaskDelete(emac->rx_task_hdl);
-    free(emac);
+	emac_w5100_t *emac = __containerof( mac, emac_w5100_t, parent );
+	vTaskDelete( emac->rx_task_hdl );
+	free( emac );
 	END;
-    return ESP_OK;
+	return ESP_OK;
 }
 
-esp_eth_mac_t *esp_eth_mac_new_w5100(const eth_mac_config_t *mac_config)
+esp_eth_mac_t *esp_eth_mac_new_w5100( const eth_mac_config_t *mac_config )
 {
 	START;
-    esp_eth_mac_t *ret = NULL;
-    emac_w5100_t *emac = NULL;
-    MAC_CHECK(mac_config, "can't set mac config to null", err, NULL);
-    emac = calloc(1, sizeof(emac_w5100_t));
-    MAC_CHECK(emac, "calloc emac failed", err, NULL);
+	esp_eth_mac_t *ret = NULL;
+	emac_w5100_t *emac = NULL;
+	MAC_CHECK( mac_config, "can't set mac config to null", err, NULL );
+	emac = calloc( 1, sizeof( emac_w5100_t ) );
+	MAC_CHECK( emac, "calloc emac failed", err, NULL );
 
-    emac->parent.set_mediator = emac_w5100_set_mediator;
-    emac->parent.init = emac_w5100_init;
-    emac->parent.deinit = emac_w5100_deinit;
-    emac->parent.del = emac_w5100_del;
-    emac->parent.write_phy_reg = emac_w5100_write_phy_reg;
-    emac->parent.read_phy_reg = emac_w5100_read_phy_reg;
-    emac->parent.set_addr = emac_w5100_set_addr;
-    emac->parent.get_addr = emac_w5100_get_addr;
-    emac->parent.set_speed = emac_w5100_set_speed;
-    emac->parent.set_duplex = emac_w5100_set_duplex;
-    emac->parent.set_link = emac_w5100_set_link;
-    emac->parent.set_promiscuous = emac_w5100_set_promiscuous;
-    emac->parent.transmit = emac_w5100_transmit;
-    emac->parent.receive = emac_w5100_receive;
+	emac->parent.set_mediator = emac_w5100_set_mediator;
+	emac->parent.init = emac_w5100_init;
+	emac->parent.deinit = emac_w5100_deinit;
+	emac->parent.del = emac_w5100_del;
+	emac->parent.write_phy_reg = emac_w5100_write_phy_reg;
+	emac->parent.read_phy_reg = emac_w5100_read_phy_reg;
+	emac->parent.set_addr = emac_w5100_set_addr;
+	emac->parent.get_addr = emac_w5100_get_addr;
+	emac->parent.set_speed = emac_w5100_set_speed;
+	emac->parent.set_duplex = emac_w5100_set_duplex;
+	emac->parent.set_link = emac_w5100_set_link;
+	emac->parent.set_promiscuous = emac_w5100_set_promiscuous;
+	emac->parent.transmit = emac_w5100_transmit;
+	emac->parent.receive = emac_w5100_receive;
 
-    BaseType_t xReturned = xTaskCreate(emac_w5100_task, "w5100_tsk", mac_config->rx_task_stack_size, emac,
-                                       mac_config->rx_task_prio, &emac->rx_task_hdl);
-    MAC_CHECK(xReturned == pdPASS, "create w5100 task failed", err, NULL);
+	BaseType_t xReturned = xTaskCreate( emac_w5100_task,
+		"w5100_tsk",
+		mac_config->rx_task_stack_size,
+		emac,
+		mac_config->rx_task_prio,
+		&emac->rx_task_hdl );
+	MAC_CHECK( xReturned == pdPASS, "create w5100 task failed", err, NULL );
 	END;
-    return &(emac->parent);
+	return &( emac->parent );
 err:
-    if (emac) {
-        if (emac->rx_task_hdl) {
-            vTaskDelete(emac->rx_task_hdl);
-        }
-        free(emac);
-    }
+	if ( emac )
+	{
+		if ( emac->rx_task_hdl )
+		{
+			vTaskDelete( emac->rx_task_hdl );
+		}
+		free( emac );
+	}
 	END;
-    return ret;
+	return ret;
 }
--- emac/include/eth_main.h
+++ emac/include/eth_main.h
@@ -9,35 +9,35 @@ extern "C" {
 #include "esp_eth_phy.h"
 
 // #define LOG_FUN(X) ESP_LOGW(TAG, X " %s:%d", __func__, __LINE__)
-#define LOG_FUN(X)
+#define LOG_FUN( X )
 
-#define START LOG_FUN("start")
-#define END LOG_FUN("end");
-
-/**
-* @brief Create w5100 Ethernet MAC instance
-*
-* @param[in] enc28j60_config: w5100 specific configuration
-* @param[in] mac_config: Ethernet MAC configuration
-*
-* @return
-*      - instance: create MAC instance successfully
-*      - NULL: create MAC instance failed because some error occurred
-*/
-esp_eth_mac_t *esp_eth_mac_new_w5100(const eth_mac_config_t *mac_config);
+#define START LOG_FUN( "start" )
+#define END	  LOG_FUN( "end" );
 
 /**
-* @brief Create a PHY instance of w5100
-*
-* @param[in] config: configuration of PHY
-*
-* @return
-*      - instance: create PHY instance successfully
-*      - NULL: create PHY instance failed because some error occurred
-*/
-esp_eth_phy_t *esp_eth_phy_new_w5100(const eth_phy_config_t *config);
+ * @brief Create w5100 Ethernet MAC instance
+ *
+ * @param[in] enc28j60_config: w5100 specific configuration
+ * @param[in] mac_config: Ethernet MAC configuration
+ *
+ * @return
+ *      - instance: create MAC instance successfully
+ *      - NULL: create MAC instance failed because some error occurred
+ */
+esp_eth_mac_t *esp_eth_mac_new_w5100( const eth_mac_config_t *mac_config );
 
-void eth_main(void);
+/**
+ * @brief Create a PHY instance of w5100
+ *
+ * @param[in] config: configuration of PHY
+ *
+ * @return
+ *      - instance: create PHY instance successfully
+ *      - NULL: create PHY instance failed because some error occurred
+ */
+esp_eth_phy_t *esp_eth_phy_new_w5100( const eth_phy_config_t *config );
+
+void eth_main( void );
 
 #ifdef __cplusplus
 }
