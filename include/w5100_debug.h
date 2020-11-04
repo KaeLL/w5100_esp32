068a19fb16c62844d0a3a6f6a97c9382166705e1
nvmd <phd.pc@hotmail.com>
2020-11-04 12:29:33 -0300

    Removed socket timeout detection
    Fixed verification of whether IR register was
    cleaned up correctly.
    
    Changed function traces from verbose to debug.
    
    Fixed reading RX_RD registers being read twice.

--- include/w5100_debug.h
+++ include/w5100_debug.h
@@ -5,5 +5,5 @@
 
 #define tag_def( X ) static const char *TAG = X
 
-#define f_entry() ESP_LOGV( TAG, "entered %s", __func__ )
-#define f_exit()  ESP_LOGV( TAG, "exited %s at %s:%d", __func__, __FILE__, __LINE__ )
+#define f_entry() ESP_LOGD( TAG, "entered %s", __func__ )
+#define f_exit()  ESP_LOGD( TAG, "exited %s at %s:%d", __func__, __FILE__, __LINE__ )
--- w5100_socket.c
+++ w5100_socket.c
@@ -34,7 +34,7 @@ void w5100_close( void )
 
 	do
 		IINCHIP_WRITE( S0_IR, 0xFF );
-	while ( !IINCHIP_READ( S0_IR ) );
+	while ( IINCHIP_READ( S0_IR ) );
 
 	f_exit();
 }
@@ -54,11 +54,7 @@ uint16_t w5100_send( uint8_t *buf, uint16_t len )
 
 	uint8_t ir_reg;
 	while ( !( ( ir_reg = IINCHIP_READ( S0_IR ) ) & S0_IR_SEND_OK ) )
-		if ( ir_reg & S0_IR_TIMEOUT )
-		{
-			f_exit();
-			return 0;
-		}
+		;
 	f_exit();
 
 	return len;
@@ -67,7 +63,7 @@ uint16_t w5100_send( uint8_t *buf, uint16_t len )
 uint16_t w5100_recv( uint8_t **buf )
 {
 	f_entry();
-	uint16_t data_len, ptr = getS0_XX_XSR( S0_RX_RD0 );
+	uint16_t data_len, ptr = read_uint16_reg( S0_RX_RD0 );
 
 	read_data( ptr, ( uint8_t * )&data_len, 2 );
 	ptr += 2;
