ea31aead177688e3b7a960b1a6f6a1b0a58b7ad4
nvmd <phd.pc@hotmail.com>
2020-11-04 14:41:54 -0300

    Added option to toggle function tracing

--- include/w5100_debug.h
+++ include/w5100_debug.h
@@ -1,9 +1,16 @@
 
 #pragma once
 
-#include "esp_log.h"
+#include "sdkconfig.h"
 
-#define tag_def( X ) static const char *TAG = X
+#if CONFIG_W5100_ENABLE_DEBUG_LOGS
+#	include "esp_log.h"
 
-#define f_entry() ESP_LOGD( TAG, "entered %s", __func__ )
-#define f_exit()  ESP_LOGD( TAG, "exited %s at %s:%d", __func__, __FILE__, __LINE__ )
+#	define tag_def( X ) static const char *TAG = X
+#	define f_entry()	 ESP_LOGD( TAG, "entered %s", __func__ )
+#	define f_exit()	 ESP_LOGD( TAG, "exited %s at %s:%d", __func__, __FILE__, __LINE__ )
+#else
+#	define tag_def( X )
+#	define f_entry()
+#	define f_exit()
+#endif
